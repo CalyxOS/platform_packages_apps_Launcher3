6d42c60ee10ee4d9887889db583fb7a19ad9efe1
Chirayu Desai <chirayu@calyxinstitute.org>
2023-03-30 22:10:10 +0000

    Revert "fixup! Pause apps feature"
    This reverts commit 1dc617d28caab5183aa64a0c73f2e535ed334234.
    
    Reason for revert: Can't unpause any app, blocked by IT admin
    
    Change-Id: Id51b824697b5d99a2eba27f4323f4adeea441da1

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -49,10 +49,6 @@ public abstract class SystemShortcut<T extends Context & ActivityContext> extend
         implements View.OnClickListener {
 
     private static final String TAG = SystemShortcut.class.getSimpleName();
-
-    /** Package name of the Android platform. */
-    private static final String PLATFORM_PACKAGE_NAME = "android";
-
     private final int mIconResId;
     protected final int mLabelResId;
     protected int mAccessibilityActionId;
@@ -284,25 +280,24 @@ public abstract class SystemShortcut<T extends Context & ActivityContext> extend
                     .setMessage(view.getContext().getString(R.string.pause_apps_dialog_message,
                             appLabel))
                     .setNegativeButton(android.R.string.cancel, null)
-                    .setPositiveButton(R.string.pause, (dialog, which) -> {
-                        try {
-                            // Suspension (app pause) must appear to come from the system,
-                            // aka PLATFORM_PACKAGE_NAME; otherwise, the suspension may be
-                            // removed by the system in some circumstances, e.g. on reboot.
-                            // See PackageManagerService#removeAllNonSystemPackageSuspensions
-                            AppGlobals.getPackageManager().setPackagesSuspendedAsUser(
-                                    new String[]{
-                                            mItemInfo.getTargetComponent().getPackageName()},
-                                    true, null, null,
-                                    new SuspendDialogInfo.Builder()
-                                            .setIcon(R.drawable.ic_hourglass_top)
-                                            .setTitle(R.string.paused_apps_dialog_title)
-                                            .setMessage(R.string.paused_apps_dialog_message)
-                                            .setNeutralButtonAction(BUTTON_ACTION_UNSUSPEND)
-                                            .build(), PLATFORM_PACKAGE_NAME,
-                                    mItemInfo.user.getIdentifier());
-                        } catch (RemoteException e) {
-                            Log.e(TAG, "Failed to pause app", e);
+                    .setPositiveButton(R.string.pause, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            try {
+                                AppGlobals.getPackageManager().setPackagesSuspendedAsUser(
+                                        new String[]{
+                                                mItemInfo.getTargetComponent().getPackageName()},
+                                        true, null, null,
+                                        new SuspendDialogInfo.Builder()
+                                                .setIcon(R.drawable.ic_hourglass_top)
+                                                .setTitle(R.string.paused_apps_dialog_title)
+                                                .setMessage(R.string.paused_apps_dialog_message)
+                                                .setNeutralButtonAction(BUTTON_ACTION_UNSUSPEND)
+                                                .build(), view.getContext().getOpPackageName(),
+                                        mItemInfo.user.getIdentifier());
+                            } catch (RemoteException e) {
+                                Log.e(TAG, "Failed to pause app", e);
+                            }
                         }
                     })
                     .show();
