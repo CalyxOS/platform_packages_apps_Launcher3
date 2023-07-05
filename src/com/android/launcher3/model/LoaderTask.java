f4a2bfc55364daff0e6b024d0a442c90eaa41909
Tommy Webb <tommy@calyxinstitute.org>
2023-07-05 15:06:36 -0400

    Work tab adjustments for multiple profiles
    * When multiple work profiles are present, always show work apps
      in the app drawer as opposed to hiding them behind a Paused screen.
      This allows a particular work profile to be activated individually
      by attempting to launch one of its apps.
    * The floating action button for turning off work apps can now turn
      them all on, too, when all work profiles are turned off.
    
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Change-Id2: I91f2f7195d3e4b9d90b19a717e438b4ef89c0aa0
    Change-Id: I564c4afb55f47e3d9463790b7c45b94b3303b39a

--- src/com/android/launcher3/model/LoaderTask.java
+++ src/com/android/launcher3/model/LoaderTask.java
@@ -25,6 +25,7 @@ import static com.android.launcher3.LauncherPrefs.SHOULD_SHOW_SMARTSPACE;
 import static com.android.launcher3.LauncherSettings.Favorites.DESKTOP_ICON_FLAG;
 import static com.android.launcher3.icons.CacheableShortcutInfo.convertShortcutsToCacheableShortcuts;
 import static com.android.launcher3.icons.cache.CacheLookupFlag.DEFAULT_LOOKUP_FLAG;
+import static com.android.launcher3.model.BgDataModel.Callbacks.FLAG_HAS_MULTIPLE_PROFILES;
 import static com.android.launcher3.model.BgDataModel.Callbacks.FLAG_HAS_SHORTCUT_PERMISSION;
 import static com.android.launcher3.model.BgDataModel.Callbacks.FLAG_PRIVATE_PROFILE_QUIET_MODE_ENABLED;
 import static com.android.launcher3.model.BgDataModel.Callbacks.FLAG_QUIET_MODE_CHANGE_PERMISSION;
@@ -810,11 +811,16 @@ public class LoaderTask implements Runnable {
         }
 
         if (Flags.enablePrivateSpace()) {
-            mBgAllAppsList.setFlags(FLAG_WORK_PROFILE_QUIET_MODE_ENABLED, isWorkProfileQuiet);
+            mBgAllAppsList.setFlags(FLAG_HAS_MULTIPLE_PROFILES,
+                    mUserManagerState.hasMultipleWorkProfiles());
+            mBgAllAppsList.setFlags(FLAG_WORK_PROFILE_QUIET_MODE_ENABLED,
+                    mUserManagerState.isAllWorkProfilesQuietModeEnabled());
             mBgAllAppsList.setFlags(FLAG_PRIVATE_PROFILE_QUIET_MODE_ENABLED, isPrivateProfileQuiet);
         } else {
             mBgAllAppsList.setFlags(FLAG_QUIET_MODE_ENABLED,
-                    mUserManagerState.isAnyProfileQuietModeEnabled());
+                    mUserManagerState.isAllProfilesQuietModeEnabled());
+            mBgAllAppsList.setFlags(FLAG_HAS_MULTIPLE_PROFILES,
+                    mUserManagerState.hasMultipleProfiles());
         }
         mBgAllAppsList.setFlags(FLAG_HAS_SHORTCUT_PERMISSION,
                 hasShortcutsPermission(mContext));
