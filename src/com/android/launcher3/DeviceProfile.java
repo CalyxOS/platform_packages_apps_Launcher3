f337adb7e2cef800d89656c72e47091f011a87a7
Michael Bestas <mkbestas@gmail.com>
2023-12-07 04:30:11 +0200

    Revert "Add support for toggling taskbar"
    This reverts commit fc6789d387911611f335cf42b1140e6a68a77fdf.
    
    Reason for revert: Conflicts with QPR1.
    
    Change-Id: Ibcebddb5a08fe68bfa1009116a059caa16ae2111

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -61,8 +61,6 @@ import com.android.launcher3.util.WindowBounds;
 import com.android.launcher3.workspace.CalculatedWorkspaceSpec;
 import com.android.launcher3.workspace.WorkspaceSpecs;
 
-import lineageos.providers.LineageSettings;
-
 import java.io.PrintWriter;
 import java.util.Locale;
 import java.util.function.Consumer;
@@ -314,9 +312,7 @@ public class DeviceProfile {
         isTablet = info.isTablet(windowBounds);
         isPhone = !isTablet;
         isTwoPanels = isTablet && isMultiDisplay;
-        boolean isTaskBarEnabled = LineageSettings.System.getInt(context.getContentResolver(),
-                LineageSettings.System.ENABLE_TASKBAR, isTablet ? 1 : 0) == 1;
-        isTaskbarPresent = isTaskBarEnabled && ApiWrapper.TASKBAR_DRAWN_IN_PROCESS;
+        isTaskbarPresent = isTablet && ApiWrapper.TASKBAR_DRAWN_IN_PROCESS;
 
         // Some more constants.
         context = getContext(context, info, isVerticalBarLayout() || (isTablet && isLandscape)
