63deef37a08d9c189b7f0d1d0c642fe9a3f6ba69
Dhina17 <dhinalogu@gmail.com>
2024-11-13 16:43:00 +0530

    fixup! Work tab adjustments for multiple profiles
    As private space is enabled in Android 15, make changes accordingly
    to support multiple work profiles.
    
    Change-Id: I91f2f7195d3e4b9d90b19a717e438b4ef89c0aa0

--- src/com/android/launcher3/model/UserManagerState.java
+++ src/com/android/launcher3/model/UserManagerState.java
@@ -36,10 +36,13 @@ public class UserManagerState {
     private final LongSparseArray<Boolean> mQuietUsersSerialNoMap = new LongSparseArray<>();
     private final SparseBooleanArray mQuietUsersHashCodeMap = new SparseBooleanArray();
 
+    private UserCache mUserCache;
+
     /**
      * Initialises the state values for all users
      */
     public void init(UserCache userCache, UserManager userManager) {
+        mUserCache = userCache;
         for (UserHandle user : userManager.getUserProfiles()) {
             long serialNo = userCache.getSerialNumberForUser(user);
             boolean isUserQuiet = userManager.isQuietModeEnabled(user);
@@ -99,4 +102,47 @@ public class UserManagerState {
         final int numProfiles = allUsers.size() - 1; // not including the parent
         return numProfiles > 1;
     }
+
+    /**
+     * Returns true if all managed work profiles have quiet mode enabled.
+     */
+    public boolean isAllWorkProfilesQuietModeEnabled() {
+        // Because the parent user is included, there will always be at least one user returned
+        // by getUserProfiles and tracked by allUsers, even if there are no managed profiles.
+        final int numProfilesIncludingParent = allUsers.size();
+        if (numProfilesIncludingParent <= 1) {
+            // There are no managed profiles, only the parent user, so we can return early.
+            return false;
+        }
+        for (int i = 0; i < numProfilesIncludingParent; i++) {
+            if (!mUserCache.getUserInfo(allUsers.valueAt(i)).isWork()) {
+                // Skip if it's not work profile
+                continue;
+            }
+            long serialNo = allUsers.keyAt(i);
+            if (!isUserQuiet(serialNo)) {
+                return false;
+            }
+        }
+        // Quiet mode is on for all work profiles.
+        return true;
+    }
+
+    public boolean hasMultipleWorkProfiles() {
+        // Because the parent user is included, there will always be at least one user returned
+        // by getUserProfiles and tracked by allUsers, even if there are no managed profiles.
+        final int numProfilesIncludingParent = allUsers.size();
+        if (numProfilesIncludingParent <= 1) {
+            // There are no managed profiles, only the parent user, so we can return early.
+            return false;
+        }
+        int workProfileCount = 0;
+        for (int i = 0; i < numProfilesIncludingParent; i++) {
+            if (mUserCache.getUserInfo(allUsers.valueAt(i)).isWork()) {
+                workProfileCount++;
+            }
+        }
+        return workProfileCount > 1;
+    }
+
 }
