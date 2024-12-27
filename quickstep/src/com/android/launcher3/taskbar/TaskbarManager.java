75f464e7ff71176c38a87394ca82a7e6c212fa28
Chirayu Desai <chirayudesai1@gmail.com>
2024-12-28 01:02:59 +0530

    Revert "Add support for toggling taskbar"
    This reverts commit 8cb672069f14d576ff9418c79631c0d14dcf8139.
    
    Reason: Broken. Constantly changing code
    
    Issue: calyxos#2867
    Change-Id: I59adae36be4c4575d966a816bec5d85eeb80c305

--- quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -79,8 +79,6 @@ import com.android.systemui.shared.system.QuickStepContract.SystemUiStateFlags;
 import com.android.systemui.unfold.UnfoldTransitionProgressProvider;
 import com.android.systemui.unfold.util.ScopedUnfoldTransitionProgressProvider;
 
-import lineageos.providers.LineageSettings;
-
 import java.io.PrintWriter;
 import java.util.StringJoiner;
 
@@ -112,9 +110,6 @@ public class TaskbarManager {
 
     private static final Uri NAV_BAR_KIDS_MODE = Settings.Secure.getUriFor(
             Settings.Secure.NAV_BAR_KIDS_MODE);
-
-    private static final Uri ENABLE_TASKBAR_URI = LineageSettings.System.getUriFor(
-            LineageSettings.System.ENABLE_TASKBAR);
 
     private final Context mContext;
     private final @Nullable Context mNavigationBarPanelContext;
@@ -159,7 +154,6 @@ public class TaskbarManager {
         }
     }
     private final SettingsCache.OnChangeListener mOnSettingsChangeListener = c -> recreateTaskbar();
-    private final SettingsCache.OnChangeListener mEnableTaskBarListener;
 
     private boolean mUserUnlocked = false;
 
@@ -315,18 +309,6 @@ public class TaskbarManager {
                 .register(USER_SETUP_COMPLETE_URI, mOnSettingsChangeListener);
         SettingsCache.INSTANCE.get(mContext)
                 .register(NAV_BAR_KIDS_MODE, mOnSettingsChangeListener);
-        mEnableTaskBarListener = c -> {
-            // Create the illusion of this taking effect immediately
-            // Also needed because TaskbarManager inits before SystemUiProxy on start
-            boolean enabled = LineageSettings.System.getInt(mContext.getContentResolver(),
-                    LineageSettings.System.ENABLE_TASKBAR, 0) == 1;
-            SystemUiProxy.INSTANCE.get(mContext).setTaskbarEnabled(enabled);
-
-            // Restart launcher
-            System.exit(0);
-        };
-        SettingsCache.INSTANCE.get(mContext)
-                .register(ENABLE_TASKBAR_URI, mEnableTaskBarListener);
         Log.d(TASKBAR_NOT_DESTROYED_TAG, "registering component callbacks from constructor.");
         mContext.registerComponentCallbacks(mComponentCallbacks);
         mShutdownReceiver.register(mContext, Intent.ACTION_SHUTDOWN);
@@ -483,10 +465,9 @@ public class TaskbarManager {
                 + " [dp != null (i.e. mUserUnlocked)]=" + (dp != null)
                 + " FLAG_HIDE_NAVBAR_WINDOW=" + ENABLE_TASKBAR_NAVBAR_UNIFICATION
                 + " dp.isTaskbarPresent=" + (dp == null ? "null" : dp.isTaskbarPresent));
-            SystemUiProxy sysui = SystemUiProxy.INSTANCE.get(mContext);
-            sysui.setTaskbarEnabled(isTaskbarEnabled);
             if (!isTaskbarEnabled || !isLargeScreenTaskbar) {
-                sysui.notifyTaskbarStatus(/* visible */ false, /* stashed */ false);
+                SystemUiProxy.INSTANCE.get(mContext)
+                    .notifyTaskbarStatus(/* visible */ false, /* stashed */ false);
                 if (!isTaskbarEnabled) {
                     return;
                 }
--- quickstep/src/com/android/quickstep/SystemUiProxy.java
+++ quickstep/src/com/android/quickstep/SystemUiProxy.java
@@ -541,18 +541,6 @@ public class SystemUiProxy implements ISystemUiProxy, NavHandle, SafeCloseable {
     }
 
     @Override
-    public void setTaskbarEnabled(boolean enabled) {
-        if (mSystemUiProxy != null) {
-            try {
-                mSystemUiProxy.setTaskbarEnabled(enabled);
-            } catch (RemoteException e) {
-                Log.w(TAG, "Failed call setTaskbarEnabled with arg: " +
-                        enabled, e);
-            }
-        }
-    }
-
-    @Override
     public void notifyTaskbarStatus(boolean visible, boolean stashed) {
         if (mSystemUiProxy != null) {
             try {
